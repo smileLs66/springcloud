2855d7fa6ff98fab978440d29a2c16095de0d04c
smileLs <45529962+smileLs66@users.noreply.github.com>
2019-08-16 11:50:31 +0800

    Update oauth2.sql

--- security-oauth-jwt-mybatisplus/src/sql/oauth2.sql
+++ security-oauth-jwt-mybatisplus/src/sql/oauth2.sql
@@ -99,29 +99,6 @@ CREATE TABLE `oauth_client_details` (
 -- ----------------------------
 INSERT INTO `oauth_client_details` VALUES ('client', null, '$2a$10$bisUEFV8o51zzNOFKLXAYu4GoiM4fXIK3i8.CNnMUNfLCrnBqlEd.', 'all', 'authorization_code,refresh_token', 'https://blog.csdn.net/qq_40198004', null, null, null, null, null);
 
--- ----------------------------
--- Table structure for oauth_client_details_copy
--- ----------------------------
-DROP TABLE IF EXISTS `oauth_client_details_copy`;
-CREATE TABLE `oauth_client_details_copy` (
-  `client_id` varchar(128) NOT NULL,
-  `resource_ids` varchar(256) DEFAULT NULL,
-  `client_secret` varchar(256) DEFAULT NULL,
-  `scope` varchar(256) DEFAULT NULL,
-  `authorized_grant_types` varchar(256) DEFAULT NULL,
-  `web_server_redirect_uri` varchar(256) DEFAULT NULL,
-  `authorities` varchar(256) DEFAULT NULL,
-  `access_token_validity` int(11) DEFAULT NULL,
-  `refresh_token_validity` int(11) DEFAULT NULL,
-  `additional_information` varchar(4096) DEFAULT NULL,
-  `autoapprove` varchar(256) DEFAULT NULL,
-  PRIMARY KEY (`client_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- ----------------------------
--- Records of oauth_client_details_copy
--- ----------------------------
-INSERT INTO `oauth_client_details_copy` VALUES ('client', null, '$2a$10$bisUEFV8o51zzNOFKLXAYu4GoiM4fXIK3i8.CNnMUNfLCrnBqlEd.', 'app', 'authorization_code', 'https://www.funtl.com', null, null, null, null, null);
 
 -- ----------------------------
 -- Table structure for oauth_client_token
