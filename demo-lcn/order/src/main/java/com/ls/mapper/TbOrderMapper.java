59804e28115eba5e115e554d1b3c2df9da35b911
v_lsslli <v_lsslli@tencent.com>
2020-03-16 16:20:39 +0800

    删除

--- demo-lcn/order/src/main/java/com/ls/mapper/TbOrderMapper.java
+++
@@ -1,9 +0,0 @@
-package com.ls.mapper;
-
-import com.ls.pojo.TbOrder;
-
-public interface TbOrderMapper {
-    int insert(TbOrder record);
-
-    int insertSelective(TbOrder record);
-}
--- demo-lcn/pay/src/main/java/com/ls/pay/service/PayService.java
+++
@@ -1,9 +0,0 @@
-package com.ls.pay.service;
-
-import com.ls.pay.pojo.TbPay;
-
-public interface PayService {
-
-  Boolean  savePayment(TbPay tbPay);
-
-}
