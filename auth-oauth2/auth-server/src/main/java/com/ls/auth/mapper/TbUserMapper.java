d099b8ad044f3c6b4601c00ab6fd48841d2189ba
v_lsslli <v_lsslli@tencent.com>
2020-03-16 16:31:52 +0800

    删除

--- auth-oauth2/auth-server/src/main/java/com/ls/auth/mapper/TbUserMapper.java
+++
@@ -1,8 +0,0 @@
-package com.ls.auth.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.ls.auth.pojo.TbUser;
-
-public interface TbUserMapper extends BaseMapper<TbUser>{
-
-}
